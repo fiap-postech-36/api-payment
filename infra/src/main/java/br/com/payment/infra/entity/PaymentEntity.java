89435747ba8e7d4bcfda3124b1e95e97d6bfa33f
Kaique <kaique_ssantos@outlook.com>
2024-11-08 12:16:35 -0300

    configuração sonar excludes config

--- infra/src/main/java/br/com/payment/infra/entity/PaymentEntity.java
+++ infra/src/main/java/br/com/payment/infra/entity/PaymentEntity.java
@@ -1,13 +1,10 @@
 package br.com.payment.infra.entity;
 
 import br.com.payment.domain.core.domain.entities.internal.StatusPayment;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @Document(collection = "payments")
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentEntity {
