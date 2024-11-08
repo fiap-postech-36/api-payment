5ee4ad8305d7c85d8d78c2ac2a3a3fa99fbf7132
Kaique <kaique_ssantos@outlook.com>
2024-11-08 17:42:56 -0300

    configuração sonar excludes config

--- domain/src/test/java/br/com/payment/domain/core/domain/exception/CoreExceptionNegocialTest.java
+++ domain/src/test/java/br/com/payment/domain/core/domain/exception/CoreExceptionNegocialTest.java
@@ -1,4 +1,4 @@
-package br.com.payment.domain.exception;
+package br.com.payment.domain.core.domain.exception;
 
 import br.com.payment.domain.core.exception.CoreExceptionNegocial;
 import org.junit.jupiter.api.Test;
--- domain/src/test/java/br/com/payment/domain/core/domain/exception/EmailInvalidExceptionTest.java
+++ domain/src/test/java/br/com/payment/domain/core/domain/exception/EmailInvalidExceptionTest.java
@@ -1,4 +1,4 @@
-package br.com.payment.domain.exception;
+package br.com.payment.domain.core.domain.exception;
 
 import br.com.payment.domain.core.exception.EmailInvalidException;
 import org.junit.jupiter.api.Test;
